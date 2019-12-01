67f8bfede116d36076284fa23a3ca429f8d20990
reynouts <rcw.versteeg@avans.nl>
2019-12-01 13:04:23 +0100

    day1 comments

--- AoC19/day1.py
+++ AoC19/day1.py
@@ -2,17 +2,18 @@ import aocutils as util
 
 
 def tests():
+    # Testcases from AoC puzzle example, expanded with own tests
     testcases = []
     testcases.append(([1969], 966))
     testcases.append(([14], 2))
     testcases.append(([100756], 50346))
     testcases.append(([y for x in (x[0] for x in testcases) for y in x], sum(x[1] for x in testcases)))
-
     for case in testcases:
         assert solve(case[0]) == case[1], "Expected {}, but got {}".format(case[1],solve(case[0]))
 
 
 def solve(data):
+    # Actual code for the puzzle. Going "horizontally" through the modules and needed fuel
     total = 0
     while len(data) > 0:
         data = [i for i in (x // 3 - 2 for x in data) if i > 0]
@@ -21,6 +22,7 @@ def solve(data):
 
 
 def main():
+    # Get data from day1, after running the testcases
     tests()
     data = [int(x) for x in util.get_input(1)]
     print("Total fuel needed {}".format(solve(data)))
